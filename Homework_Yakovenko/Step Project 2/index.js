617dab975d3a410ceb899bfccc2ad16e28f8a146
Kristina Yakovenko <kristinayakovenko@Kristinas-MacBook-Pro.local>
2025-10-10 00:05:53 -0500

    Fix duplicate express declaration and test cleanup

--- Homework_Yakovenko/Step Project 2/index.js
+++ Homework_Yakovenko/Step Project 2/index.js
@@ -1,25 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 80;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  //res.status(404).send('Hello World!');
-});
-
-var server = app.listen(port, () => {
-  console.log('App listening');
-});
-
-module.exports = server;
-
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Jenkins pipeline!");
+  res.status(200).send("Hello World!");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const server = app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
-module.exports = app;
+module.exports = server;
